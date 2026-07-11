3775d5661bc7bf5a025a0f94b478608efb590a85
MagicGordon <19465870+MagicGordon@users.noreply.github.com>
2026-07-11 08:33:50 +0800

    Improve Leo test runnables

--- languages/leo/runnables.scm
+++ languages/leo/runnables.scm
@@ -1,11 +1,3 @@
 (test_function_declaration
   name: (identifier) @run @test
   (#set! tag leo-test))
-
-(function_declaration
-  name: (identifier) @run @function
-  (#set! tag leo-run))
-
-(transition_declaration
-  name: (identifier) @run @function
-  (#set! tag leo-run))
